bd16fe5ea35f0584d21ef8cd0da87d58d9e9fdc2
Léo <nachezleo@gmail.com>
2026-03-06 11:25:25 +0100

    Création formulaire Unite

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Unite;
+use App\Form\UniteType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,9 +19,22 @@ class BaseController extends AbstractController
     }
 
     #[Route('/unite', name: 'app_unite')]
-	public function unite(): Response
-	{
-		return $this->render('base/unite.html.twig', [
-		]);
-	}
+    public function unite(Request $request, EntityManagerInterface $em): Response
+    {
+        $unite = new Unite();
+        $form = $this->createForm(UniteType::class, $unite);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($unite);
+                $em->flush();
+                $this->addFlash('notice', 'Message envoyé');
+                return $this->redirectToRoute('app_unite');
+            }
+        }
+        return $this->render('base/unite.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
