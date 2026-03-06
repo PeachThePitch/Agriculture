5341ad505ae4e61724e4bd67e09b10eb1733d6b1
PeachThePitch <safiatouier@gmail.com>
2026-03-06 10:24:29 +0100

    Mise en place des composants

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace App\Controller;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    class BaseController extends AbstractController
+    {
+    #[Route('/', name: 'app_accueil')] // /base est l’URL de la page, name est le nom de la route
+    public function index(): Response
+    {
+    return $this->render('base/index.html.twig', [ 
+    ]);
+    }
+    }
