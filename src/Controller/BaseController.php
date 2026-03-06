1ec45d68a1610203bf48f057d6947fd6798d224e
Léo <nachezleo@gmail.com>
2026-03-06 11:04:38 +0100

    création page Unité

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,14 +1,23 @@
 <?php
-    namespace App\Controller;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
-    class BaseController extends AbstractController
-    {
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
     #[Route('/', name: 'app_accueil')] // /base est l’URL de la page, name est le nom de la route
     public function index(): Response
     {
-    return $this->render('base/index.html.twig', [ 
-    ]);
+        return $this->render('base/index.html.twig', [
+        ]);
     }
-    }
+
+    #[Route('/unite', name: 'app_unite')]
+	public function unite(): Response
+	{
+		return $this->render('base/unite.html.twig', [
+		]);
+	}
+}
