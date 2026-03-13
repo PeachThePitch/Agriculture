07de0db417ba85f2c2c220d74ab6dfeb575ce640
Léo <nachezleo@gmail.com>
2026-03-13 11:29:57 +0100

    Ajout table culture

---
+++ src/Entity/Culture.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CultureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CultureRepository::class)]
+class Culture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateDebut = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateFin = null;
+
+    #[ORM\Column]
+    private ?int $qteRecolte = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cultures')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Parcelle $parcelle = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cultures')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Production $production = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTime $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTime
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTime $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getQteRecolte(): ?int
+    {
+        return $this->qteRecolte;
+    }
+
+    public function setQteRecolte(int $qteRecolte): static
+    {
+        $this->qteRecolte = $qteRecolte;
+
+        return $this;
+    }
+
+    public function getParcelle(): ?Parcelle
+    {
+        return $this->parcelle;
+    }
+
+    public function setParcelle(?Parcelle $parcelle): static
+    {
+        $this->parcelle = $parcelle;
+
+        return $this;
+    }
+
+    public function getProduction(): ?Production
+    {
+        return $this->production;
+    }
+
+    public function setProduction(?Production $production): static
+    {
+        $this->production = $production;
+
+        return $this;
+    }
+}
