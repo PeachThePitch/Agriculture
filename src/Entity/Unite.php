40b10ba3f65d338bfa575549c497f5ac378ae00b
PeachThePitch <safiatouier@gmail.com>
2026-03-13 10:58:56 +0100

    Ajout boutons suppression + modifications Parcelle

--- src/Entity/Unite.php
+++ src/Entity/Unite.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UniteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UniteRepository::class)]
@@ -15,6 +17,17 @@ class Unite
 
     #[ORM\Column]
     private ?int $unite = null;
+
+    /**
+     * @var Collection<int, ElementChimique>
+     */
+    #[ORM\OneToMany(targetEntity: ElementChimique::class, mappedBy: 'unite', orphanRemoval: true)]
+    private Collection $elementChimiques;
+
+    public function __construct()
+    {
+        $this->elementChimiques = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Unite
     public function setUnite(int $unite): static
     {
         $this->unite = $unite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ElementChimique>
+     */
+    public function getElementChimiques(): Collection
+    {
+        return $this->elementChimiques;
+    }
+
+    public function addElementChimique(ElementChimique $elementChimique): static
+    {
+        if (!$this->elementChimiques->contains($elementChimique)) {
+            $this->elementChimiques->add($elementChimique);
+            $elementChimique->setUnite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElementChimique(ElementChimique $elementChimique): static
+    {
+        if ($this->elementChimiques->removeElement($elementChimique)) {
+            // set the owning side to null (unless already changed)
+            if ($elementChimique->getUnite() === $this) {
+                $elementChimique->setUnite(null);
+            }
+        }
 
         return $this;
     }
