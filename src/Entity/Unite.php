9f29bc5d3d327844b7d1ee85659da3e02b51e55d
Léo <nachezleo@gmail.com>
2026-03-13 10:58:40 +0100

    Ajout des tables production et engrais

--- src/Entity/Unite.php
+++ src/Entity/Unite.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UniteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UniteRepository::class)]
@@ -15,6 +17,24 @@ class Unite
 
     #[ORM\Column]
     private ?int $unite = null;
+
+    /**
+     * @var Collection<int, Engrais>
+     */
+    #[ORM\OneToMany(targetEntity: Engrais::class, mappedBy: 'unite', orphanRemoval: true)]
+    private Collection $engrais;
+
+    /**
+     * @var Collection<int, Production>
+     */
+    #[ORM\OneToMany(targetEntity: Production::class, mappedBy: 'unite')]
+    private Collection $productions;
+
+    public function __construct()
+    {
+        $this->engrais = new ArrayCollection();
+        $this->productions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +49,66 @@ class Unite
     public function setUnite(int $unite): static
     {
         $this->unite = $unite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Engrais>
+     */
+    public function getEngrais(): Collection
+    {
+        return $this->engrais;
+    }
+
+    public function addEngrai(Engrais $engrai): static
+    {
+        if (!$this->engrais->contains($engrai)) {
+            $this->engrais->add($engrai);
+            $engrai->setUnite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEngrai(Engrais $engrai): static
+    {
+        if ($this->engrais->removeElement($engrai)) {
+            // set the owning side to null (unless already changed)
+            if ($engrai->getUnite() === $this) {
+                $engrai->setUnite(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Production>
+     */
+    public function getProductions(): Collection
+    {
+        return $this->productions;
+    }
+
+    public function addProduction(Production $production): static
+    {
+        if (!$this->productions->contains($production)) {
+            $this->productions->add($production);
+            $production->setUnite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduction(Production $production): static
+    {
+        if ($this->productions->removeElement($production)) {
+            // set the owning side to null (unless already changed)
+            if ($production->getUnite() === $this) {
+                $production->setUnite(null);
+            }
+        }
 
         return $this;
     }
